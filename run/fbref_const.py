cb1c1d4d54af047f8f3e7150643e6aa6a92485ad
1111111huang <huangtianqi0819@hotmail.com>
2025-06-18 21:45:56 -0400

    Try MLP fitter

--- run/fbref_const.py
+++ run/fbref_const.py
@@ -18,4 +18,8 @@ URLs={
     '2021-22': "https://fbref.com/en/comps/9/2021-2022/schedule/2021-2022-Premier-League-Scores-and-Fixtures",
     '2020-21': "https://fbref.com/en/comps/9/2020-2021/schedule/2020-2021-Premier-League-Scores-and-Fixtures",
     '2019-20': "https://fbref.com/en/comps/9/2019-2020/schedule/2019-2020-Premier-League-Scores-and-Fixtures",
+    '2018-19': "https://fbref.com/en/comps/9/2018-2019/schedule/2018-2019-Premier-League-Scores-and-Fixtures",
+    '2017-18': "https://fbref.com/en/comps/9/2017-2018/schedule/2017-2018-Premier-League-Scores-and-Fixtures",
+    '2016-17': "https://fbref.com/en/comps/9/2016-2017/schedule/2016-2017-Premier-League-Scores-and-Fixtures",
+    '2015-16': "https://fbref.com/en/comps/9/2015-2016/schedule/2015-2016-Premier-League-Scores-and-Fixtures",  
 }
