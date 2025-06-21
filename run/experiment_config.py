8b0a1b74dc0decb3b0b3b8131348e2abe7f3a4af
1111111huang <huangtianqi0819@hotmail.com>
2025-06-21 19:41:20 -0400

    Changing experiment config format

--- run/experiment_config.py
+++ run/experiment_config.py
@@ -1,3 +1,4 @@
+from const import PROCESSED_DATA_PATH, FEATURES_PATH
 TARGET_RANGES={
     'home_goals': [0,5],
     'away_goals': [0,6],
@@ -11,6 +12,21 @@ TARGET_RANGES={
     #'away_sots':[1, 10],
 }
 
+SEASONS=[
+    '2017-18',
+    '2018-19',
+    '2019-20',
+    '2020-21',
+    '2021-22',
+    '2022-23',
+    '2023-24',
+    '2024-25',
+]
+
+COMPETITION = 'premier_league'
+COMP_PROCESSED_DATA_PATH=f'{PROCESSED_DATA_PATH}/{COMPETITION}/'
+COMP_FEATURES_PATH=f'{FEATURES_PATH}/{COMPETITION}/'
+
 TEST_SIZE = 0.1
 VALIDATION_SIZE = 0.2
 RANDOM_STATE = 42
@@ -18,3 +34,16 @@ RANDOM_STATE = 42
 EXPERIMENT_NAME = "mlflow_distribution_ts_5fold_0.5train_2017-24"
 
 KEY_COLUMNS=['home', 'away', 'date']
+
+TRAINGING_CONFIG = {
+    'seasons': SEASONS,
+    'target_ranges': TARGET_RANGES,
+    'competition': COMPETITION,
+    'processed_data_path': COMP_PROCESSED_DATA_PATH,
+    'features_path': COMP_FEATURES_PATH,
+    'test_size': TEST_SIZE,
+    'validation_size': VALIDATION_SIZE,
+    'random_state': RANDOM_STATE,
+    'experiment_name': EXPERIMENT_NAME,
+    'key_columns': KEY_COLUMNS
+}
